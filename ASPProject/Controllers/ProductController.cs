48d180fc2127feb550cede3b932a85956df43902
Brett Andrews <brettandrews3@gmail.com>
2020-05-11 12:27:23 -0500

    HTML Product Models Written

--- ASPProject/Controllers/ProductController.cs
+++ ASPProject/Controllers/ProductController.cs
@@ -10,10 +10,19 @@ namespace ASPProject.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly IProductRepository repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            this.repo = repo;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var products = repo.GetAllProducts();
+
+            return View(products);
         }
     }
 }
---
+++ ASPProject/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System;
+using ASPProject.Models;
+
+namespace ASPProject
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
