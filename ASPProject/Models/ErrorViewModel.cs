82d7e5050c339b8bb384b0a51b04ba6f83986b45
Brett Andrews <brettandrews3@gmail.com>
2020-05-11 09:20:49 -0500

    first commit

---
+++ ASPProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
